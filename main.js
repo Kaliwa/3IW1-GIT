265687f667ab98fd8413e16ce248e4351a86ba94
Adrien-Gardien <gardienadrien@gmail.com>
2024-01-22 17:03:53 +0100

    ajout du main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function ajouterDeux(a, b) {
   return a + b;
 }
 
-console.log('La somme est : ', ajouterDeux(nombre, 3));
+console.log("La somme est : ", ajouterDeux(nombre, 3));
 
-if (nombre == '42') {
-  console.log('Le nombre est une chaîne.');
+if (nombre == "42") {
+  console.log("Le nombre est une chaîne.");
 }
 
 
